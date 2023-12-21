88f88f542e0c922e0355a3343f24137a3908aaf7
Polina Bozina <polinabozina678@gmail.com>
2023-12-21 12:05:51 +0100

    com

--- main.py
+++
@@ -1,9 +0,0 @@
-name = str(input("Name: "))
-age = int(input("Age: "))
-has_driver_licence = input("has_driver_licence: ")
-
-if name and age >= 18 and has_driver_licence == True:
-    print(f"User {name} can rent a car")
-else:
-    print(f"User {name} can't rent a car")
-    
